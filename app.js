d9ef5317825d49c1f253895292a1c0a325e05f70
Gaz <gaz@Gazs-MacBook-Pro.local>
2023-06-04 12:16:35 +0100

    full app dogfood commit

---
+++ app.js
@@ -0,0 +1,23 @@
+import fs from "fs";
+import csv from "csvtojson";
+import { Parser } from "json2csv";
+import gurmukhiUtils from "gurmukhi-utils";
+import { log } from "console";
+
+(async () => {
+  const flashcards = await csv({ delimiter: "\t" }).fromFile(
+    "3000PunjabiWords-Gurmukhi.csv"
+  );
+
+  for (let i = 0; i < flashcards.length; i++) {
+    const gurmukhiTranslation = flashcards[i]["punjabi word"];
+    const shahmukhiTranslation = gurmukhiUtils.toShahmukhi(gurmukhiTranslation);
+
+    flashcards[i][
+      "punjabi word"
+    ] = `${gurmukhiTranslation}\n${shahmukhiTranslation}`;
+  }
+
+  const newCSV = new Parser().parse(flashcards);
+  fs.writeFileSync("3000PunjabiWords", newCSV);
+})();
